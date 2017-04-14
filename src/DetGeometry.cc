647c1840246b67536f61356a65a6f30370c51cdd
idalov <dep24cpp@yandex.ru>
2017-04-14 22:47:26 +0300

    init V1

--- src/DetGeometry.cc
+++ src/DetGeometry.cc
@@ -1,5 +1,7 @@
 
 #include <G4PhysicalConstants.hh>
+#include <G4VisAttributes.hh>
+#include <G4Sphere.hh>
 #include "DetGeometry.hh"
 
 DetGeometry::DetGeometry() {
@@ -14,7 +16,14 @@ DetGeometry::DetGeometry() {
 DetGeometry::~DetGeometry() {}
 
 G4VPhysicalVolume* DetGeometry::Construct(){
-
+    G4Box * gamma_box = new G4Box("g_b", 100*mm, 100*mm, 100*mm);
+    G4LogicalVolume* log_gamma_box = new G4LogicalVolume(gamma_box, material, "log_g_b");
+    log_gamma_box->SetVisAttributes(G4Color(0.0, 0.60, 1.0));
+    new G4PVPlacement(0, G4ThreeVector(0,0,-200),log_gamma_box,"pl_g_b",logicWorld,false,0);
+    G4Sphere * beta_sphere = new G4Sphere("b_s",0,150,0,360*deg,0,360*deg);
+    G4LogicalVolume* log_beta_sphere = new G4LogicalVolume(beta_sphere, material, "log_g_b");
+    log_beta_sphere->SetVisAttributes(G4Color(1.0, 0.40, 0.30));
+    new G4PVPlacement(0, G4ThreeVector(0,-300,200),log_beta_sphere,"pl_g_b",logicWorld,false,0);
     G4cout<<"Geometry of detector is build successfully\t\t\t\t\t\tOK!!!"<<G4endl;
     return physWorld;
 }
