bb20209dd299607afc29611a5eac97c938be9e13
idalov <dep24cpp@yandex.ru>
2017-04-14 22:51:46 +0300

    init V2

--- src/DetGeometry.cc
+++ src/DetGeometry.cc
@@ -16,14 +16,14 @@ DetGeometry::DetGeometry() {
 DetGeometry::~DetGeometry() {}
 
 G4VPhysicalVolume* DetGeometry::Construct(){
-    G4Box * gamma_box = new G4Box("g_b", 100*mm, 100*mm, 100*mm);
-    G4LogicalVolume* log_gamma_box = new G4LogicalVolume(gamma_box, material, "log_g_b");
-    log_gamma_box->SetVisAttributes(G4Color(0.0, 0.60, 1.0));
-    new G4PVPlacement(0, G4ThreeVector(0,0,-200),log_gamma_box,"pl_g_b",logicWorld,false,0);
-    G4Sphere * beta_sphere = new G4Sphere("b_s",0,150,0,360*deg,0,360*deg);
-    G4LogicalVolume* log_beta_sphere = new G4LogicalVolume(beta_sphere, material, "log_g_b");
-    log_beta_sphere->SetVisAttributes(G4Color(1.0, 0.40, 0.30));
-    new G4PVPlacement(0, G4ThreeVector(0,-300,200),log_beta_sphere,"pl_g_b",logicWorld,false,0);
+    G4Box * beta_box = new G4Box("b_b", 100*mm, 100*mm, 100*mm);
+    G4LogicalVolume* log_beta_box = new G4LogicalVolume(beta_box, material, "log_b_b");
+    log_beta_box->SetVisAttributes(G4Color(1.0, 0.40, 0.30));
+    new G4PVPlacement(0, G4ThreeVector(-100,0,200),log_beta_box,"pl_b_b",logicWorld,false,0);
+    G4Sphere * gamma_sphere = new G4Sphere("g_s",0,130,0,360*deg,0,360*deg);
+    G4LogicalVolume* log_gamma_sphere = new G4LogicalVolume(gamma_sphere, material, "log_g_s");
+    log_gamma_sphere->SetVisAttributes(G4Color(0.0, 1.00, 0.0));
+    new G4PVPlacement(0, G4ThreeVector(0,-300,200),log_gamma_sphere,"pl_g_s",logicWorld,false,0);
     G4cout<<"Geometry of detector is build successfully\t\t\t\t\t\tOK!!!"<<G4endl;
     return physWorld;
 }
